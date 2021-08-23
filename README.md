106da724f8b2158672c5b0b337a18866989756f9
Andrew Rothstein <andrew.rothstein@gmail.com>
2021-08-22 20:08:10 -0400

    tilt:0.22.5

---
+++ README.md
@@ -0,0 +1,40 @@
+andrewrothstein.tilt
+=========
+
+![Build Status](https://github.com/andrewrothstein/ansible-tilt/actions/workflows/build.yml/badge.svg)
+
+Installs [tilt](https://github.com/tilt-dev/tilt).
+
+Requirements
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Role Variables
+--------------
+
+See [defaults/main.yml](defaults/main.yml)
+
+Dependencies
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Example Playbook
+----------------
+
+```yml
+- hosts: servers
+  roles:
+    - andrewrothstein.tilt
+```
+
+License
+-------
+
+MIT
+
+Author Information
+------------------
+
+Andrew Rothstein <andrew.rothstein@gmail.com>
---
+++ dl-checksums.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env sh
+set -e
+DIR=~/Downloads
+APP=tilt
+GHUSER=tilt-dev
+MIRROR=https://github.com/${GHUSER}/${APP}/releases/download
+
+dl() {
+    local ver=$1
+    local lchecksums=$2
+    local os=$3
+    local arch=$4
+    local archive_type=${5:-tar.gz}
+    local platform="${os}.${arch}"
+    local file="${APP}.${ver}.${platform}.${archive_type}"
+    local url="$MIRROR/v$ver/$file"
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(grep $file $lchecksums | awk '{print $1}')
+}
+
+dl_ver() {
+    local ver=$1
+    # https://github.com/tilt-dev/tilt/releases/download/v0.22.5/checksums.txt
+    local url="$MIRROR/v$ver/checksums.txt"
+    local lchecksums="$DIR/${APP}_${ver}_checksums.txt"
+    if [ ! -e $lchecksums ];
+    then
+        wget -q -O $lchecksums $url
+    fi
+
+    printf "  # %s\n" $url
+    printf "  '%s':\n" $ver
+
+    dl $ver $lchecksums mac arm64_ALPHA
+    dl $ver $lchecksums mac x86_64
+    dl $ver $lchecksums linux arm_ALPHA
+    dl $ver $lchecksums linux arm64_ALPHA
+    dl $ver $lchecksums linux x86_64
+    dl $ver $lchecksums windows x86_64 zip
+}
+
+dl_ver ${1:-0.22.5}
